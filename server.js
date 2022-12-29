049a34e9523c4a95eab4494db84d95b9c1f80ad1
ongchunheng <ocheng.90@gmail.com>
2022-12-29 15:44:25 +0800

    added get with sampels

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("h")
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  console.log('Here')
+
+  // success with json
+  // res.json({ message: "Success" })
+
+  // error with json
+  // res.status(500).json({ message: "Error" })
+
+  // download file
+  // res.download("server.js")
+
+  // render html file
+  res.render("index", { text: "World" })
+
+  // for testing purposes, use .send()
+  // res.send("hi")
+})
+
+app.listen(3000)
